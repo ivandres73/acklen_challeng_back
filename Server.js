44aa7aa8156c3ccdd628f4f0495cc01111788d6d
Ivan Carcamo <ivandres73@unitec.edu>
2019-11-25 13:40:36 -0600

    changed to port 80

--- Server.js
+++ Server.js
@@ -1,7 +1,7 @@
 const express = require('express'); 
 const User = require('./User');  
 const app = express();
-const PORT = 3001;
+const PORT = 80;
 
 app.get('/', (req, res) => {
   res.send({ message: 'endpoint working' });
