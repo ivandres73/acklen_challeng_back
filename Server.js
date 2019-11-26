414191a4df88950a0df0e8b9bd693d5a62f5cf34
Ivan Carcamo <ivandres73@unitec.edu>
2019-11-26 08:58:27 -0600

    CORS error handling

--- Server.js
+++ Server.js
@@ -3,6 +3,17 @@ const User = require('./User');
 const app = express();
 const PORT = 80;
 
+//CORS HANDLING BEFORE ANY GET/POST/PUT...
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');//i can change only to aws pc
+  res.header('Access-Control-Allow-Headres', 'Origin, X-Requested-Width, Content-Type, Accept, Authorization');
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+    return res.status(200).json();
+  }
+  next();
+})
+
 app.get('/', (req, res) => {
   res.send({ message: 'endpoint working' });
 });
