af0bc48ca8fbc4741f702b340c7dde3f02f39898
Ivan Carcamo <ivandres73@unitec.edu>
2019-11-26 09:06:41 -0600

    typo fix

--- Server.js
+++ Server.js
@@ -6,7 +6,7 @@ const PORT = 80;
 //CORS HANDLING BEFORE ANY GET/POST/PUT...
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');//i can change only to aws pc
-  res.header('Access-Control-Allow-Headres', 'Origin, X-Requested-Width, Content-Type, Accept, Authorization');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-Width, Content-Type, Accept, Authorization');
   if (req.method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
     return res.status(200).json();
